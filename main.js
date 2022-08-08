7ac852d06e07de14398246b361c0e3078ea10654
unknown <moetechnologies1@gmail.com>
2022-08-08 13:45:54 +0100

    modify commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var orangediv = document.getElementById('orangediv')
+
+
+orangediv.addEventListener('click', function(){
+    alert('orange div is selected')
+})
+
+orangediv.style.background =  "blue"
+
